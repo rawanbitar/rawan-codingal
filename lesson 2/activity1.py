a076cf4a34952b3b98d45df6069588b21f88a889
rawanbitar <rawanbitar38@gmail.com>
2025-02-27 20:41:34 +0400

    lesson 2

---
+++ lesson 2/activity1.py
@@ -0,0 +1,10 @@
+# variable = value
+name= "rawan"
+age="13"
+skills="drawing"
+school="AMB school"
+
+print(f"hi my name is {name}")
+print(f"my age is {age}")
+print(f"my skills are{skills}")
+print(f"my school name is {school}")
